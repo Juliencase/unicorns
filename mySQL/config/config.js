75bfa0f37144358b02292aba14c1b13ab45d1c26
Juliencase <36259545+Juliencase@users.noreply.github.com>
2018-11-14 16:07:49 +0100

    Update config.js

--- mySQL/config/config.js
+++ mySQL/config/config.js
@@ -3,6 +3,6 @@ var mysql = require('mysql')
 exports.connection = {
               host     : 'localhost',
               user     : 'root',
-              password : 'Megalithe1234',
+              password : '',
               database : 'article'
 };
